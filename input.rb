644a6ece53a7380a40effc641b85d46783f4a8c5
SatyaGayathri <satyagayathri1111@gmail.com>
2024-07-24 15:20:02 +0530

    input is updated

--- input.rb
+++ input.rb
@@ -2,3 +2,7 @@ puts "Enter your name:"
 
 name=gets.chomp()
 puts("Hello"+name+"How are you?")  #this gives output as hello name and how are you in next line ,so avoid this we use chomp()
+
+puts "Enter age:"
+age=gets.chomp()
+puts("Hello"+name+". You are "+age+" old")
